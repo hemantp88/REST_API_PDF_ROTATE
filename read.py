1ec92a2aa38e5d420aef1cd902b68421eb0d59b2
hemantp88 <panchalhemant030700@gmail.com>
2022-11-07 15:56:08 +0530

    initial setup configuration

---
+++ read.py
@@ -0,0 +1,43 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from typing import Union
+from pydantic import BaseModel
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class params(BaseModel):
+    pdf:str = None
+    rotate:int = None
+    page:int = None
+
+
+@app.post("/rotate")
+def read_user(body: params):
+    body=body.dict()
+    return  rotatepdf(body["pdf"],body["rotate"],body["page"])
+
+
+def rotatepdf(file_path:str,angle_of_rotation:int,page_number:int) :
+    targetfile = file_path
+    outfile = open("/tmp/result.pdf", "wb")
+    pdf = PdfFileReader(targetfile)
+    writer = PdfFileWriter()
+    numPages = pdf.getNumPages()
+    num = page_number -1
+    angle = angle_of_rotation % 360
+    for i in range(numPages):
+        if i == num :
+            page = pdf.getPage(i)
+            page.rotateClockwise(angle)
+            writer.addPage(page)
+        else :
+            page = pdf.getPage(i)
+            page.rotateClockwise(0)
+            writer.addPage(page)
+
+    writer.write(outfile)
+    outfile.close()
+    return {"file_path" :"/tmp/result.pdf","angle_of_rotation":angle,"page_number":page_number}
+    
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+else:
+        page = pdf.getPage(0)
+        page.rotateClockwise(0)
+    
